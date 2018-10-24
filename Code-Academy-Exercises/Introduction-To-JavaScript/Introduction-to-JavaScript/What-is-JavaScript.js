a61a326f16488f861aedcc116a261f4768602fae
gijolopez <glopez22@me.com>
2018-10-24 16:41:08 -0600

    Into to JavaScript review.

--- Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/What-is-JavaScript.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/What-is-JavaScript.js
@@ -4,3 +4,18 @@ Last year, millions of learners from our community started with JavaScript. Why?
 Since JavaScript remains at the core of web development, it's often the first language learned by self-taught coders eager to learn and build. We're excited for what you'll be able to create with the JavaScript foundation you gain here. JavaScript powers the dynamic behavior on most websites, including this one.
 
 In this lesson, you will learn introductory coding concepts including data types and built-in objects— essential knowledge for all aspiring developers. Make sure to take notes and pace yourself. This foundation will set you up for understanding the more complex concepts you'll encounter later.
+
+
+Review
+Let's take one more glance at the concepts we just learned:
+
+Data is printed, or logged, to the console, a panel that displays messages, with console.log().
+You can write single-line comments with // and multi-line comments between /* and */.
+There are 7 fundamental data types in JavaScript: strings, numbers, booleans, null, undefined, symbol, and object.
+Numbers are any number without quotes: 23.8879
+Strings are characters wrapped in single or double quotes: 'Sample String'
+The built-in arithmetic operators include +, -, *, /, and %.
+Objects, including instances of data types, can have properties, stored information. The properties are denoted with a . after the name of the object, for example: 'Hello'.length.
+Objects, including instances of data types, can have methods which perform actions. Methods are called by appending the object or instance with a period, the method name, and parentheses. For example: 'hello'.toUpperCase().
+We can access properties and methods by using the ., dot operator.
+Built-in objects, including Math, are collections of methods and properties that JavaScript provides.
