7079cde5e2cbec9c0a93b581c44efb8e4b358685
gijolopez <glopez22@me.com>
2018-10-25 18:56:57 -0600

    Added a review of Scope.

--- Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Scope.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Scope.js
@@ -5,3 +5,20 @@ An important idea in programming is scope. Scope defines where variables can be 
 You can think of scope like the view of the night sky from your window. Everyone who lives on the planet Earth is in the global scope of the stars. The stars are accessible globally. Meanwhile, if you live in a city, you may see the city skyline or the river. The skyline and river are only accessible locally in your city, but you can still see the stars that are available globally.
 
 Over the next few exercises, we'll explore how scope relates to variables and learn best practices for variable declaration.
+
+
+SCOPE
+Review: Scope
+In this lesson, you learned about scope and how it impacts the accessibility of different variables.
+
+Let's review the following terms:
+
+Scope is the idea in programming that some variables are accessible/inaccessible from other parts of the program.
+Blocks are statements that exist within curly braces {}.
+Global scope refers to the context within which variables are accessible to every part of the program.
+Global variables are variables that exist within global scope.
+Block scope refers to the context within which variables that are accessible only within the block they are defined.
+Local variables are variables that exist within block scope.
+Global namespace is the space in our code that contains globally scoped information.
+Scope pollution is when too many variables exist in a namespace or variable names are reused.
+As you continue your coding journey, remember to use best practices when declaring your variables! Scoping your variables tightly will ensure that your code has clean, organized, and modular logic.
