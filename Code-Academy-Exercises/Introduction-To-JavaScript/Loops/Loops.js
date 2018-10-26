bb86d084bb465d8195ea16707a43b9887c4af162
gijolopez <glopez22@me.com>
2018-10-25 20:30:06 -0600

    Added a review of the loops section.

--- Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Loops.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Loops.js
@@ -4,4 +4,17 @@ A loop is a programming tool that repeats a set of instructions until a specifie
 
 When we need to reuse a task in our code, we often bundle that action in a function. Similarly, when we see that a process has to repeat multiple times in a row, we write a loop. Loops allow us to create efficient code that automates processes to make scalable, manageable programs.
 
+LOOPS
+Review
+Great job! In this lesson, we learned how to write cleaner code with loops. You now know:
+
+Loops perform repetitive actions so we don’t have to code that process manually every time.
+How to write for loops with an iterator variable that increments or decrements
+How to use a for loop to iterate through an array
+A nested for loop is a loop inside another loop
+while loops allow for different types of stopping conditions
+Stopping conditions are crucial for avoiding infinite loops.
+do...while loops run code at least once— only checking the stopping condition after the first execution
+The break keyword allows programs to leave a loop during the execution of its block
+
 As illustrated in the diagram, loops iterate or repeat an action until a specific condition is met. When the condition is met, the loop stops and the computer moves on to the next part of the program.
