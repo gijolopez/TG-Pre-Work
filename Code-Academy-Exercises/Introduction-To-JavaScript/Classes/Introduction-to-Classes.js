36c6a2e98f3e675e59c9fe7b136f1a5faaa07a36
gijolopez <glopez22@me.com>
2018-10-27 15:52:28 -0600

    Added a review of Classes.

--- Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Introduction-to-Classes.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Introduction-to-Classes.js
@@ -1,8 +1,3 @@
-/*
-CLASSES
-Introduction to Classes
-JavaScript is an object-oriented programming (OOP) language we can use to model real-world items. In this lesson, you will learn how to make classes. Classes are a tool that developers use to quickly produce similar objects.
-*/
 
 
 class Dog {
@@ -29,3 +24,21 @@ console.log(halley.behavior); // Print behavior value to console
 halley.incrementBehavior(); // Add one to behavior
 console.log(halley.name); // Print name value to console
 console.log(halley.behavior); // Print behavior value to console
+/*
+CLASSES
+Introduction to Classes
+JavaScript is an object-oriented programming (OOP) language we can use to model real-world items. In this lesson, you will learn how to make classes. Classes are a tool that developers use to quickly produce similar objects.
+
+CLASSES
+Review: Classes
+Way to go! Let's review what you learned.
+
+Classes are templates for objects.
+Javascript calls a constructor method when we create a new instance of a class.
+Inheritance is when we create a parent class with properties and methods that we can extend to child classes.
+We use the extends keyword to create a subclass.
+The super keyword calls the constructor() of a parent class.
+Static methods are called on the class, but not on instances of the class.
+In completing this lesson, you've taken one step closer to writing efficient, production-level JavaScript. Good luck as you continue to develop your skills and move into intermediate-level concepts.
+
+*/
