30a2dc60517b1663f06eb0d671eb482e1ec5be2b
gijolopez <glopez22@me.com>
2018-10-28 21:05:35 -0600

    Added a review on Intermediate Javascript Modules.

--- Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/Hello-Modules.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/Hello-Modules.js
@@ -10,3 +10,18 @@ reuse and recycle defined logic in different parts of our application;
 keep information private and protected from other modules;
 and, importantly, prevent pollution of the global namespace and potential naming collisions, by cautiously selecting variables and behavior we load into a program.
 */
+
+INTERMEDIATE JAVASCRIPT MODULES
+Review
+We just learned how to use JavaScript modules. Let's review what we learned:
+
+Modules in JavaScript are reusable pieces of code that can be exported from one program and imported for use in another program.
+
+module.exports exports the module for use in another program.
+require() imports the module for use in the current program.
+ES6 introduced a more flexible, easier syntax to export modules:
+
+default exports use export default to export JavaScript objects, functions, and primitive data types.
+named exports use the export keyword to export data in variables.
+named exports can be aliased with the as keyword.
+import is a keyword that imports any object, function, or data type.
