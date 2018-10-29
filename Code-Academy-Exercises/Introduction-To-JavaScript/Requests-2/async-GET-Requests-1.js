cdff8ca1622923b66309ded6c7006339a37bf121
gijolopez <glopez22@me.com>
2018-10-29 07:38:43 -0600

    the next and final is to incorporate that experience and logic into making a real request.

--- Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/async-GET-Requests-1.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/async-GET-Requests-1.js
@@ -1,11 +1,18 @@
+/*
+
 REQUESTS II
-fetch() GET Requests I
-The first type of requests we’re going to tackle are GET requests using fetch()
+async GET Requests I
+Let’s take a minute to appreciate what you’ve accomplished so far:
 
-MDN: Fetch API.
-The fetch() function:
+used fetch() to make GET and POST requests.
+check the status of the responses coming back
+catch errors that might possibly arise
+taking successful responses and rendering it on the webpage
+That is fantastic! It’s the basis of how the internet works!
 
-Creates a request object that contains relevant information that an API needs.
-Sends that request object to the API endpoint provided.
-Returns a promise that ultimately resolves to a response object, which contains the status of the promise with information the API sent back.
-In the next exercise we’ll go over the boilerplate code for using fetch() and walk through what each step does!
+In the following exercises, we’re going to take what you’ve learned about chaining Promises and make it simpler using functionality introduced in ES8: async and await. You read that right, you did the hard part already, now it’s time to make it easier.
+
+The structure for this request will also be slightly different. Notice the new keywords async and await, as well as the try and catch statements.
+
+We’ll be going over how to write the boilerplate code for async GET requests in the next lesson.
+*/
--- Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/async-GET-Requests-2.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/async-GET-Requests-2.js
@@ -1,9 +1,12 @@
 const getData = async () => {
-  try {const response = await fetch('https://api-to-call.com/endpoint')
-  if (response.ok) {
-   const jsonResponse = await response.json();
-    return jsonResponse;
-  } throw new Error ('Request failed!');
-
-  } catch (error) {console.log(error);}
-};
+  try {
+    const response = await fetch('https://api-to-call.com/endpoint');
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error('Request failed!');
+  } catch (error) {
+    console.log(error);
+  }
+}
